6500043cbf4c544c3b866824561e3b9fdfeb704a
Lukas Mares <lukasmares138@gmail.com>
2026-04-23 15:27:33 +0200

    Projití sort a sorted

--- sorting.py
+++ sorting.py
@@ -8,6 +8,9 @@ def random_numbers(count, low=0, high=100):
 
 
 
+# ----------------------------------------------------------------------------------------------
+# Moje definované funkce
+
 def selection_sort(numbers):
     numbers = numbers.copy()            # udělám kopii vstupu, abych ho nezměnil
     for krok in range(len(numbers)):
@@ -45,6 +48,37 @@ def bubble_sort(numbers):
 
 
 
+# ---------------------------------------------------------------------------
+# Zkoušky výstupů
+
+# list.sort() - seřadí původní seznam, nic nevrací
+my_list_sort = [3, 8, 1, 2, 32]
+my_list_sort.sort()
+print(my_list_sort)   # [1, 2, 3, 8, 32]
+
+
+# sorted(list) - vytvoří se nový seznam, který je třeba předepsat,
+# v něm bude seřazen obsah původního seznamu
+my_list_sort_sorted = [3, 8, 1, 2, 32]
+new_list = sorted(my_list_sort_sorted)
+print(my_list_sort_sorted)   # [3, 8, 1, 2, 32]   ← beze změny
+print(new_list)  # [1, 2, 3, 8, 32]
+
+
+# argument reverse=True - seřadí sestupně
+my_list_reverse = [3, 8, 1, 2, 32]
+my_list = sorted(my_list_reverse, reverse=True)
+print(my_list)   # [32, 8, 3, 2, 1]
+
+
+# argument key=... - nastavuje klíč, podle kterého řadíme
+list_of_words = ["MOO", "meeeoow", "woof", "BZZZZZZ"]
+list_of_words_sorted = sorted(list_of_words, key=len)
+print(list_of_words_sorted)   # ['MOO', 'woof', 'meeeoow', 'BZZZZZZ']
+list_of_words_lower = sorted(list_of_words, key=str.lower)
+print(list_of_words_lower)      # ['BZZZZZZ', 'meeeoow', 'MOO', 'woof']
+
+
 
 
 
@@ -58,9 +92,9 @@ if __name__ == "__main__":
     # print(selection_sort([5, 1, 4, 2, 8, 100, 1, -2, 0]))
     # print(selection_sort(values))
 
-    print("Výsledky Bubble Sort:")
+    # print("Výsledky Bubble Sort:")
     # print(bubble_sort([5, 1, 4, 2, 8, 100, 1, -2, 0]))
-    print(bubble_sort(values))
+    # print(bubble_sort(values))
 
 
 
